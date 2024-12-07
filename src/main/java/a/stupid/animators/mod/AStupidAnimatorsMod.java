5cbe7fbe704a5c4538fc9e053a166604e1e90438
A-Stupid-Animator <izzzyroddy@outlook.com>
2024-12-07 08:33:45 -0800

    a important comment/ not really though

---
+++ src/main/java/a/stupid/animators/mod/AStupidAnimatorsMod.java
@@ -0,0 +1,16 @@
+package a.stupid.animators.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//important comment
+public class AStupidAnimatorsMod implements ModInitializer {
+	public static final String MOD_ID = "a-stupid-animators-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
