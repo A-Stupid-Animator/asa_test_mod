5b18eb3017461303306512a2832b9a9630ebc253
A-Stupid-Animator <izzzyroddy@outlook.com>
2024-12-07 08:37:52 -0800

    Now this is REALLY imprtant i hope it tranfers correctly

---
+++ src/main/java/a/stupid/animators/mod/AStupidAnimatorsModClient.java
@@ -0,0 +1,10 @@
+package a.stupid.animators.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AStupidAnimatorsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/a/stupid/animators/mod/AStupidAnimatorsModDataGenerator.java
@@ -0,0 +1,11 @@
+package a.stupid.animators.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AStupidAnimatorsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
